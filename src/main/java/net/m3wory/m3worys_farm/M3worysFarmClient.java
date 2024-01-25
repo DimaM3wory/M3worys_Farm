e4728d174d056d57fa6265ce46bd4b10d2225e2f
DimaM3wory <m3wory19@gmail.com>
2024-01-25 11:25:37 +0300

    Initial commit

---
+++ src/main/java/net/m3wory/m3worys_farm/M3worysFarmClient.java
@@ -0,0 +1,10 @@
+package net.m3wory.m3worys_farm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class M3worysFarmClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/m3wory/m3worys_farm/M3worysFarmDataGenerator.java
@@ -0,0 +1,11 @@
+package net.m3wory.m3worys_farm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class M3worysFarmDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
